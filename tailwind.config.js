14fbe55e9c04eb5c162e3466dd7acc9fd042f6f0
Silviu Miron <silviu_miron_90@outlook.com>
2024-04-09 23:49:34 +0300

    Build :Add media-quesies,

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ['./docs/**/*.{html,js}'],
   theme: {
+    screens: {
+      sm: { min: '50px', max: '870px' },
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      md: { min: '871px', max: '2600px' },
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      // lg: { min: "1024px", max: "1279px" },
+      // // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      // xl: { min: "1280px", max: "1535px" },
+      // // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      // "2xl": { min: "1536px" },
+      // // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       colors: {
         // primary text
