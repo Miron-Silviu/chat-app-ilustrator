505b21f40a14d0dec64a17f6e2426ce8585d957a
Silviu Miron <silviu_miron_90@outlook.com>
2024-04-26 00:41:54 +0300

    Update message class : Add svg image and create layout according to guidline .

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,8 +20,6 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        image:
-          '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg>',
         'radial-gradient':
           'radial-gradient(50.40% 70.55%  at 1% 6.29%, hsl(276, 55%, 52%)  100%, hsl(270, 20%, 96%), transparent)',
         radialGradient:
